4950a9fc08de6955cb53da72efe055cae4da4234
Magmatic <sbrocketman80@gmail.com>
2021-10-02 00:20:33 -0700

    Add XP reward to escape loot, tweak GGC rewards, fix Rats day 2 rewards

--- blxp.lua
+++ blxp.lua
@@ -1,8 +1,9 @@
 levels = {"kenaz", "welcome_to_the_jungle_1_night", "welcome_to_the_jungle_1", "branchbank", "mex", "sah", "dah", 
 "firestarter_1", "firestarter_2", "firestarter_3", "four_stores", "nightclub", "fish", "ukrainian_job", "alex_must_die_2",
 "dinner", "arm_cro", "arm_hcm", "arm_fac", "arm_par", "arm_und", "election_day_2", "election_day_3", "bex", "pex", "fex"}
+escapes = {"escape_cafe_day", "escape_park_day", "escape_cafe", "escape_park", "escape_street", "escape_overpass", "escape_overpass_night", "escape_garage"}
 loot = {
-kenaz = {money = 500, painting = 500}, 
+kenaz = {money = 500, painting = 1000, unknown = 250}, 
 welcome_to_the_jungle_1_night = {money = 1000, coke = 1000, weapon = 1000},
 welcome_to_the_jungle_1 = {money = 1000, coke = 1000, weapon = 1000},
 branchbank = {money = 1000, gold = 1000},
@@ -16,7 +17,7 @@ four_stores = {jewelry = 1000},
 nightclub = {money = 500, coke = 1000},
 fish = {mus_artifact = 1000},
 ukrainian_job = {diamonds = 500},
-alex_must_die_2 = {money = 1000},
+alex_2 = {money = 1000},
 dinner = {din_pig = 1000},
 arm_cro = {gold = 500, money = 500, jewelry = 500},
 arm_hcm = {gold = 500, money = 500, jewelry = 500},
@@ -33,8 +34,9 @@ fex = {coke = 500, weapon = 100}
 Hooks:PostHook(LootManager, "sync_secure_loot", "sync", function(self, carry_id)
 	if has_value(levels, managers.job:current_level_id()) then
 		managers.experience:mission_xp_award(loot[Global.level_data.level_id][carry_id] or 0)
+	elseif has_value(escapes, managers.job:current_level_id()) then
+		managers.experience:mission_xp_award(500)
 	end
-end)
 
 function has_value (tab, val)
     for index, value in ipairs(tab) do
